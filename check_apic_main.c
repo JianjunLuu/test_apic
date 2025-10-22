ddf5efd1fbc0326bfb88c37fcd7a68408c712e08
Lu Jianjun <133227670+JianjunLuu@users.noreply.github.com>
2025-10-22 20:56:30 +0800

    Add files via upload

--- check_apic_main.c
+++ check_apic_main.c
@@ -31,27 +31,14 @@
 
 MODULE_LICENSE("GPL");
 
-#define IA32_APIC_BASE_MSR 0x1B
-#define X2APIC_VERSION_REG 0x803
-#define X2APIC_LVT_TIMER   0x832
-#define X2APIC_TIMER_INIT  0x838
-#define X2APIC_TIMER_CUR   0x839  // Current Count Register
-#define X2APIC_EOI        0x80B
-#define X2APIC_DIV_CONF 0x83E
-
-#define TIMER_VECTOR 0xEf  // 中断向量,239
-#define IA32_PMC0    0x0C1   // PMC0 MSR
-
-#define TIMER_INIT_COUNT   0x100  // 初始计数值，可调整
-
-
-//保存原状态
-/*static gate_desc old_gate;
-static bool old_gate_saved = false;
-static u64 old_lvt_timer = 0;
-static u64 old_init_count = 0;
-static bool old_lvt_saved = false;*/
-
+#define APIC_EOI                    0x80b
+#define APIC_ICR                    0x830
+#define APIC_LVTT                   0x832
+#define APIC_TMICT                  0x838
+#define APIC_TDCR                   0x83e
+#define APIC_LVTT_ONESHOT      (0 << 17)
+#define APIC_LVTT_PERIODIC (1 << 17)
+#define IRQ_VECTOR                  45
 extern void my_idt_stub(void);
 
 static unsigned long long irq_count = 0;//记录中断处理函数被调用次数
@@ -73,7 +60,7 @@ EXPORT_SYMBOL(apic_timer_handler);
 //在 CPU0 上触发中断
 void trigger_on_cpu0(void *info)
 {
-    asm volatile("int $0xEF");
+    asm volatile("int $0x2d");
 
 }
 
@@ -202,11 +189,11 @@ typedef struct gate_struct gate_desc;
 
 
 //以下是直接修改 idt_table 的方法写入idt
-    //gate_desc *desc = &idt_table[TIMER_VECTOR];// 指针指向要改的元素地址
+    //gate_desc *desc = &idt_table[IRQ_VECTOR];// 指针指向要改的元素地址
 
 //修改idt副本 new_idt
-    gate_desc *desc = &new_idt[TIMER_VECTOR];// 指针指向新副本要改的元素地址
-    pr_info("指针指向要改的元素地址 (entry[%02x]) address = %px\n", TIMER_VECTOR, desc);
+    gate_desc *desc = &new_idt[IRQ_VECTOR];// 指针指向新副本要改的元素地址
+    pr_info("指针指向要改的元素地址 (entry[%02x]) address = %px\n", IRQ_VECTOR, desc);
   
     // 1. 禁止中断修改
     local_irq_disable();
@@ -222,14 +209,14 @@ typedef struct gate_struct gate_desc;
     desc->reserved      = 0;
     // 3. 恢复中断
     local_irq_enable();
-    pr_info("[HOOK] IDT[%02x] hooked to 0x%lx\n",  TIMER_VECTOR, handler_addr);
+    pr_info("[HOOK] IDT[%02x] hooked to 0x%lx\n",  IRQ_VECTOR, handler_addr);
 //验证写入结果
-    gate_desc *verify_desc = &new_idt[TIMER_VECTOR];
+    gate_desc *verify_desc = &new_idt[IRQ_VECTOR];
         unsigned long new_offset = ((unsigned long)verify_desc->offset_high << 32) |
                                    ((unsigned long)verify_desc->offset_middle << 16) |
                                    verify_desc->offset_low;
 
-        pr_info("[验证写入结果] IDT[%02x] 描述符内容:\n", TIMER_VECTOR);
+        pr_info("[验证写入结果] IDT[%02x] 描述符内容:\n", IRQ_VECTOR);
         pr_info("  offset = 0x%016lx\n", new_offset);
         pr_info("  segment = 0x%04x\n", verify_desc->segment);
         pr_info("  type = 0x%x, dpl = %u, p = %u, ist = %u\n",
@@ -254,12 +241,6 @@ typedef struct gate_struct gate_desc;
     // 使用 lidt 加载新的 IDT到寄存器
     write_idt(&idtr);
     pr_info("[IDT] 已加载新的 IDT 副本，地址 = 0x%lx\n", idtr.address);
-
-
-
-    //old_gate = idt_table[TIMER_VECTOR];
-    //old_gate_saved = true;
-
 
 //以下是使用 pack_gate 和 write_idt_entry 的方法写入idt
     //pack_gate(&g, GATE_INTERRUPT, (unsigned long)my_idt_stub,
@@ -289,161 +270,58 @@ typedef struct gate_struct gate_desc;
     //write_cr0(read_cr0() | X86_CR0_WP);
 
     pr_info("cpu %d: IDT vector 0x%x -> my_idt_stub installed\n",
-            smp_processor_id(), TIMER_VECTOR);
+            smp_processor_id(), IRQ_VECTOR);
 }
 
-//恢复IDT
-/*static void restore_idt_entry_on_cpu(void *arg)
+
+static inline int apic_write2(uint32_t reg, uint32_t v)
+    {
+        wrmsrl(reg, v);
+        return 0;
+    }
+
+static inline uint32_t apic_read2(uint32_t reg)
+    {
+    
+        u64 val;
+        rdmsrl(reg, val);  // 读取 MSR
+        return (uint32_t)val; // 取低32位
+    }
+int apic_timer_oneshot(uint8_t vector)
 {
-    struct desc_ptr idtr;
-    gate_desc *idt_table;
+    apic_write2(APIC_LVTT, vector | APIC_LVTT_ONESHOT);
+    //apic_write(APIC_TDCR, APIC_TDR_DIV_2);
+    pr_info("APIC LVTT = 0x%llx",
+            apic_read2(APIC_LVTT));
+    return 0;
 
-    if (!old_gate_saved)
-        return;
+}
 
-    store_idt(&idtr);
-    idt_table = (gate_desc *)idtr.address;
 
-    write_cr0(read_cr0() & (~X86_CR0_WP));
-    write_idt_entry(idt_table, TIMER_VECTOR, &old_gate);
-    write_cr0(read_cr0() | X86_CR0_WP);
 
-    pr_info("cpu %d: IDT vector 0x%x restored\n",
-            smp_processor_id(), TIMER_VECTOR);
-}*/
 static void setup_x2apic_timer_on_cpu(void *info){
-    u32 eax, ebx, ecx, edx;
-    u64 apic_base;
-    u64 value;
-    bool x2apic_supported;
-    // 读取 IA32_APIC_BASE MSR
-    rdmsrl(IA32_APIC_BASE_MSR, apic_base);
-    pr_info("IA32_APIC_BASE MSR: 0x%llx\n", apic_base);
-    pr_info("APIC Global Enable (bit11): %llu\n", (apic_base >> 11) & 1);
-    pr_info("x2APIC mode (EXTD bit10): %llu\n", (apic_base >> 10) & 1);
-
-    // 读取 Local APIC Version
-    rdmsrl(X2APIC_VERSION_REG, value);
-    pr_info("x2APIC Version Register: 0x%llx\n", value);
-    pr_info("Local APIC Version: 0x%llx\n", value & 0xFF);
-    pr_info("Max LVT Entry: %llu\n", (value >> 16) & 0xFF);
 
     // 注册中断处理函数  
-    smp_call_function_single(0, install_idt_entry_on_cpu, NULL, 1);
-    //install_idt_entry_on_cpu(NULL);
+    //smp_call_function_single(0, install_idt_entry_on_cpu, NULL, 1);
+    install_idt_entry_on_cpu(NULL);
 
+    //配置apic
+    apic_timer_oneshot(IRQ_VECTOR);
 
-    // 读取 LVT Timer 寄存器
-    rdmsrl(X2APIC_LVT_TIMER, value);
-    pr_info("LVT Timer Register (设置前): 0x%llx\n", value);    
-    u8 vector;
-    u8 mask;
-    u8 mode;
-    vector = value & 0xFF;          // 0-7 位
-    mask   = (value >> 16) & 0x1;   // 16 位
-    mode   = (value >> 17) & 0x3;   // 17-18 位
-
-    pr_info("向量 (0-7 bits): 0x%x\n", vector);
-    pr_info("Mask (16): 0x%x\n", mask);
-    pr_info("模式 (17-18 bits): 0x%x\n", mode);
-
-    // 清除原有向量号 (bits 0–7)
-    //value &= ~0xFFULL;
-    // 写入新的 TIMER_VECTOR
-    //value |= TIMER_VECTOR;
-    // 清除原来的17-18位（Timer Mode）
-    value &= ~(0x3ULL << 17);
-    // 设置为Periodic (01)
-    value |= (0x1ULL << 17);
-    // 写回寄存器
-    wrmsrl(X2APIC_LVT_TIMER, value);
-
-    // 再次读取确认
-    rdmsrl(X2APIC_LVT_TIMER, value);
-    pr_info("LVT Timer Register (设置完周期模式和vector后): 0x%llx\n", value);
-    vector = value & 0xFF;          // 0-7 位
-    mask   = (value >> 16) & 0x1;   // 16 位
-    mode   = (value >> 17) & 0x3;   // 17-18 位
-
-    pr_info("向量 修改后(0-7 bits): 0x%x\n", vector);
-    pr_info("Mask 修改后(16): 0x%x\n", mask);
-    pr_info("模式 修改后(17-18 bits): 0x%x\n", mode);
-
-   
-    // 设置 Divide Configuration Register
-    // 1. 读取设置前的寄存器值
-    rdmsrl(X2APIC_DIV_CONF, value);
-    pr_info("Divide Configuration Register (设置前): 0x%llx\n", value);
-    /*
-     Divide Value 对应 bits 0,1,3
-     例如要设置为 “Divide by 16” => 011b
-     */
-    value &= ~0xB;   // 清除 bits [3,1,0] (bit3=0b1000, bit1=0b0010, bit0=0b0001)
-    value |= 0x3;    // 设置为 011b => Divide by 16
-    // 2. 写入新的配置值
-    wrmsrl(X2APIC_DIV_CONF, value);
-    // 3. 读取设置后的寄存器值
-    rdmsrl(X2APIC_DIV_CONF, value);
-    pr_info("Divide Configuration Register (设置后): 0x%llx\n", value);
-
-
-    // 写入 Initial Count 启动定时器
-    wrmsrl(X2APIC_TIMER_INIT, TIMER_INIT_COUNT);
-    rdmsrl(X2APIC_TIMER_INIT, value);
-    pr_info("Initial Count Register: 0x%llx\n", value);
-    
-    // 读取 Current Count
-    rdmsrl(X2APIC_TIMER_CUR, value);
-    pr_info("等待前Current Count Register: 0x%llx\n", value);
-    
-    pr_info("等待 LAPIC Timer 中断触发中...\n");
-    // 延迟等待中断触发
-    //msleep(3000);
-
-    // 读取 Current Count
-    rdmsrl(X2APIC_TIMER_CUR, value);
-    pr_info("等待后Current Count Register: 0x%llx\n", value);
-
-    //确认计时器循环
-    for (int i = 0; i < 5; i++) {
-        rdmsrl(X2APIC_TIMER_CUR, value);
-        pr_info("[CPU%d] Current Count: 0x%llx\n", smp_processor_id(), value);
-        //msleep(500);
-    }
-
-
-    // 停止定时器：设置 Initial Count 为 0或者屏蔽 LVT Timer
-    //rdmsrl(X2APIC_LVT_TIMER, value);
-    // 设置 Mask 位 (bit 16 = 1)
-    //value |= (1ULL << 16);
-    //wrmsrl(X2APIC_LVT_TIMER, value);
-
-    //wrmsrl(X2APIC_TIMER_INIT, 0x0); 
+    //开始计时
+    apic_write2(APIC_TMICT, 10);
 
 }
+
+
 static int __init check_x2apic_timer_init(void)
 {
-    u32 eax, ebx, ecx, edx;
-    u64 apic_base;
-    u64 value;
-    bool x2apic_supported;
 
-    
-    // 检测 CPU 是否支持 x2APIC
-    cpuid(1, &eax, &ebx, &ecx, &edx);
-    x2apic_supported = (ecx & (1 << 21)) ? true : false;
-    if (!x2apic_supported) {
-        pr_info("CPU does NOT support x2APIC\n");
-        return -ENODEV;
-    }
-    pr_info("CPU supports x2APIC\n");
-
-    
     // 在 CPU0 上设置 x2APIC 定时器
     smp_call_function_single(0, setup_x2apic_timer_on_cpu, NULL, 1);
 // 在 CPU0 上触发
-    smp_call_function_single(0, trigger_on_cpu0, NULL, 1);
-    smp_call_function_single(0, trigger_on_cpu0, NULL, 1);
+    //smp_call_function_single(0, trigger_on_cpu0, NULL, 1);
+    //smp_call_function_single(0, trigger_on_cpu0, NULL, 1);
     return 0;
 }
 
