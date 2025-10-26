21356c9f9541d0cc00ffa3c58d795dd172ccc61a
Lu Jianjun <133227670+JianjunLuu@users.noreply.github.com>
2025-10-26 13:50:32 +0800

    Add files via upload

--- check_apic_main.c
+++ check_apic_main.c
@@ -301,7 +301,7 @@ static inline uint32_t apic_read2(uint32_t reg)
 int apic_timer_oneshot(uint8_t vector)
 {
     apic_write2(APIC_LVTT, vector | APIC_LVTT_ONESHOT);
-    //apic_write2(APIC_TDCR, APIC_TDR_DIV_2);
+    apic_write2(APIC_TDCR, APIC_TDR_DIV_2);
     pr_info("cpu %d, APIC LVTT = 0x%llx",smp_processor_id(),
             apic_read2(APIC_LVTT));
     return 0;
@@ -351,11 +351,11 @@ static void setup_x2apic_timer_on_cpu(void *info){
     install_idt_entry_on_cpu(NULL);
 
     //配置oneshot模式
-    apic_timer_oneshot(IRQ_VECTOR);
-/*
+    //apic_timer_oneshot(IRQ_VECTOR);
+
     //配置周期模式
     apic_timer_periodic(IRQ_VECTOR);
-    //apic_write2(APIC_TMICT, 100);
+    apic_write2(APIC_TMICT, 10);
     //验证apic计时器
     rdmsrl(X2APIC_TIMER_INIT, value);
     pr_info("Initial Count Register: 0x%llx\n", value);
@@ -365,13 +365,15 @@ static void setup_x2apic_timer_on_cpu(void *info){
         pr_info("Current Count Register 0x%llx\n", value);
     }
     //写入0停止周期模式计时器
-    //apic_write2(APIC_TMICT, 0);
+    apic_write2(APIC_TMICT, 0);
     rdmsrl(X2APIC_TIMER_CUR, value);
+    pr_info("Initial Count Register: 0x%llx\n", value);
+    rdmsrl(X2APIC_TIMER_INIT, value);
     pr_info("Current Count Register 0x%llx\n", value);
     pr_info("IRQ fired: %d, count=%d, TSC=%llu\n", __ss_irq_fired, __ss_irq_count,nemesis_tsc_aex);
-*/
+
     //测试oneshot模式中断
-    do_irq_apic_tmr_test();
+    //do_irq_apic_tmr_test();
 
 
 }
